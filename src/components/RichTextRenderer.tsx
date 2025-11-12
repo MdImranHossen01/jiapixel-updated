4ad7054723184ffd79fc4776e8404bbeb9d0d5c6
MdImranHossen01 <imranshuvo101@gmail.com>
2025-11-12 09:49:14 +0600

    dfafd

--- src/components/RichTextRenderer.tsx
+++ src/components/RichTextRenderer.tsx
@@ -1,7 +1,8 @@
-'use client';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
 
-import { useMemo } from 'react';
-import DOMPurify from 'dompurify';
+import { useMemo } from "react";
+import DOMPurify from "dompurify";
 
 interface RichTextRendererProps {
   content: string;
@@ -9,39 +10,70 @@ interface RichTextRendererProps {
   maxLines?: number; // Add maxLines prop for line-clamp
 }
 
-export default function RichTextRenderer({ 
-  content, 
-  className = '', 
-  maxLines 
+export default function RichTextRenderer({
+  content,
+  className = "",
+  maxLines,
 }: RichTextRendererProps) {
   // Use useMemo to sanitize HTML content and avoid unnecessary re-sanitization
   const sanitizedContent = useMemo(() => {
     // Only sanitize if content exists
-    if (!content) return '';
-    
+    if (!content) return "";
+
     return DOMPurify.sanitize(content, {
       ALLOWED_TAGS: [
-        'p', 'br', 'strong', 'em', 'u', 's', 'mark', 'code', 'pre', 
-        'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ul', 'ol', 'li', 
-        'a', 'blockquote', 'hr', 'img', 'span', 'div'
+        "p",
+        "br",
+        "strong",
+        "em",
+        "u",
+        "s",
+        "mark",
+        "code",
+        "pre",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+        "ul",
+        "ol",
+        "li",
+        "a",
+        "blockquote",
+        "hr",
+        "img",
+        "span",
+        "div",
       ],
       ALLOWED_ATTR: [
-        'href', 'target', 'rel', 'class', 'style', 'src', 'alt', 
-        'width', 'height', 'data-color'
+        "href",
+        "target",
+        "rel",
+        "class",
+        "style",
+        "src",
+        "alt",
+        "width",
+        "height",
+        "data-color",
       ],
     });
   }, [content]);
 
   // Create style object for line-clamp if maxLines is provided
-  const style = maxLines ? {
-    display: '-webkit-box',
-    WebkitLineClamp: maxLines,
-    WebkitBoxOrient: 'vertical',
-    overflow: 'hidden'
-  } : {};
+  const style = maxLines
+    ? {
+        display: "-webkit-box",
+        WebkitLineClamp: maxLines,
+        WebkitBoxOrient: "vertical" as any,
+        overflow: "hidden",
+      }
+    : {};
 
   return (
-    <div 
+    <div
       className={`prose prose-lg max-w-none ${className}`}
       style={style}
       dangerouslySetInnerHTML={{ __html: sanitizedContent }}
