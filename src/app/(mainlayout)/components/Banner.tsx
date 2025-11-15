bd3825d244434ff4181e3e72b58012967b6a299b
MdImranHossen01 <imranshuvo101@gmail.com>
2025-11-15 22:38:19 +0600

    banner background updated

--- src/app/(mainlayout)/components/Banner.tsx
+++ src/app/(mainlayout)/components/Banner.tsx
@@ -6,8 +6,8 @@ import { BackgroundLines } from "@/components/ui/background-lines";
 
 const Banner = () => {
   return (
-    <BackgroundLines className="h-auto">
-    // 1. Main container: Relative for positioning, bg-background for theme
+    <BackgroundLines>
+    {/* // 1. Main container: Relative for positioning, bg-background for theme */}
     <section className="relative  text-foreground pt-32 pb-20 md:pt-48 md:pb-32 text-center overflow-hidden">
       {/* 2. Background Effects (Grid and Gradient) */}
      
