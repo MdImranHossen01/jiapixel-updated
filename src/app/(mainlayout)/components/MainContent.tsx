d08e516eaba519999b9c01d5ff733c6de46b9f16
MdImranHossen01 <imranshuvo101@gmail.com>
2025-11-20 10:14:27 +0600

    lazyload

---
+++ src/app/(mainlayout)/components/MainContent.tsx
@@ -0,0 +1,19 @@
+import PortfoilioSection from "./PortfoilioSection";
+import HoverServices from "./HoverServices";
+import WhyChooseUs from "./whyChooseUs";
+import WebFeatures from "./WebFeatures";
+import FaqSection from "./faq/FaqSection";
+
+const MainContent = () => {
+  return (
+    <>
+      <PortfoilioSection/>
+      <HoverServices/>
+      <WhyChooseUs/>
+      <WebFeatures/>
+      <FaqSection/>
+    </>
+  );
+};
+
+export default MainContent;
--- src/app/(mainlayout)/page.tsx
+++ src/app/(mainlayout)/page.tsx
@@ -1,21 +1,26 @@
-
-
+import { lazy, Suspense } from 'react';
 import Banner from "./components/banner/Banner";
-import FaqSection from "./components/faq/FaqSection";
-import HoverServices from "./components/HoverServices";
-import PortfoilioSection from "./components/PortfoilioSection";
-import WebFeatures from "./components/WebFeatures";
-import WhyChooseUs from "./components/whyChooseUs";
+
+// Group components that appear together
+const MainContent = lazy(() => import("./components/MainContent"));
+
+// Loading component
+const LoadingFallback = () => (
+  <div className="min-h-[400px] flex items-center justify-center">
+    <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
+  </div>
+);
 
 const HomePage = () => {
   return (
     <div>
+      {/* Banner is critical - load immediately */}
       <Banner/>
-       <PortfoilioSection/>
-       <HoverServices/>
-      <WhyChooseUs/>
-      <WebFeatures/>
-   <FaqSection/>
+      
+      {/* Lazy load the rest of the content */}
+      <Suspense fallback={<LoadingFallback />}>
+        <MainContent />
+      </Suspense>
     </div>
   );
 };
