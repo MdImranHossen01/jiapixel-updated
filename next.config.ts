337527e73836e6d20fa7ae51580e2ed2f9fbbf07
MdImranHossen01 <imranshuvo101@gmail.com>
2026-01-10 12:59:00 +0600

    pwa implemented

--- next.config.ts
+++ next.config.ts
@@ -96,4 +96,12 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+// @ts-ignore
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+export default withPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -18,7 +18,19 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+export const viewport: Viewport = {
+  themeColor: "#000000",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
+
 export const metadata: Metadata = {
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Jia Pixel",
+  },
   metadataBase: new URL("https://www.jiapixel.com"),
   title: {
     default: "Jia Pixel | Leading Digital Agency in Bangladesh",
