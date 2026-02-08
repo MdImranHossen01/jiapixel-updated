a55020f7e05593084c031105857a2b36c29f1e18
MdImranHossen01 <imranshuvo101@gmail.com>
2026-02-08 17:51:33 +0600

    fix auth optional

--- src/hooks/useAuthModal.ts
+++ src/hooks/useAuthModal.ts
@@ -7,8 +7,8 @@ interface ServiceTier {
   title: string;
   description: string;
   price: number;
-  deliveryDays: number;
-  revisions: number;
+  deliveryDays?: number;
+  revisions?: number;
 }
 
 interface UseAuthModalReturn {
